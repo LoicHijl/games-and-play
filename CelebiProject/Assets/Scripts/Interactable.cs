59411bf8f097fde49eeb6f84f0c5697de0382a7d
marnix14 <44201873+marnix14@users.noreply.github.com>
2018-10-18 15:51:43 +0200

    Update with shooting & interactable state change

---
+++ CelebiProject/Assets/Scripts/Interactable.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public int timeState;
+   
+
+    public void TransferEnergy()
+    {
+        timeState++;
+        Debug.Log(timeState);
+            
+    }
+   
+
+}
