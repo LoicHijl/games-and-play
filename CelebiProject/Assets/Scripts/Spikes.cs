96aef5b0a9fe7de96c4a0c3eabf85ca6ca64b3a4
corvusumbra <chinnolavin@gmail.com>
2018-10-24 11:30:15 +0200

    Spike death done , starting health bar

---
+++ CelebiProject/Assets/Scripts/Spikes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+
+    private Player player;
+
+    void Start() {
+
+        player = FindObjectOfType<Player>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player")) {
+
+            player.DamagePlayer(40);
+        }
+
+
+    }
+
+
+
+}
